2f77191f49715bb3470123fb4be315bd34111a35
pipols83 <juanra.fernandez@gmail.com>
2015-03-02 18:18:02 +0100

    lista funcionando

--- app/src/main/java/com/areas/jr/areas/AreaAdapter.java
+++ app/src/main/java/com/areas/jr/areas/AreaAdapter.java
@@ -1,24 +1,41 @@
 package com.areas.jr.areas;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 108001 on 02/03/2015.
  */
 public class AreaAdapter  extends BaseAdapter {
 
-    @Override
-    public int getCount() {
-        // TODO Auto-generated method stub
-        return 0;
+    List<AreaInfo> areasList = getDataForListView();
+    private final Activity context;
+
+    public AreaAdapter(Activity context) {
+       // super(context, R.layout.activity_main_area, itemname);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
     }
 
     @Override
-    public Object getItem(int arg0) {
+    public int getCount() {
         // TODO Auto-generated method stub
-        return null;
+        return areasList.size();
+    }
+
+    @Override
+    public AreaInfo getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return areasList.get(arg0);
     }
 
     @Override
@@ -30,7 +47,44 @@ public class AreaAdapter  extends BaseAdapter {
     @Override
     public View getView(int arg0, View arg1, ViewGroup arg2) {
         // TODO Auto-generated method stub
-        return null;
-    }
+        if(arg1==null)
+        {
+            LayoutInflater inflater=context.getLayoutInflater();
+            arg1=inflater.inflate(R.layout.area_row, null,true);
 
+           // LayoutInflater inflater = (LayoutInflater) context. ListViewWithBaseAdapter.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+           // arg1 = inflater.inflate(R.layout.area_row, arg2,false);
+        }
+
+        TextView chapterName = (TextView)arg1.findViewById(R.id.textView1);
+        TextView chapterDesc = (TextView)arg1.findViewById(R.id.textView2);
+
+        AreaInfo area = areasList.get(arg0);
+
+        chapterName.setText(area.name);
+        chapterDesc.setText(area.name);
+
+        return arg1;
+    }
+    public List<AreaInfo> getDataForListView()
+    {
+        List<AreaInfo> areaList = new ArrayList<AreaInfo>();
+
+        for(int i=0;i<10;i++)
+        {
+
+            AreaInfo area = new AreaInfo();
+            area.id = i;
+            area.name = "Repsol";
+            area.gas_station = 1;
+            area.garage = 1;
+            area.restaurant = 1;
+            area.situation = 0;
+            //area.position.latitude = 20.0;
+
+            areaList.add(area);
+        }
+
+        return areaList;
+    }
 }
--- app/src/main/java/com/areas/jr/areas/MainAreaActivity.java
+++ app/src/main/java/com/areas/jr/areas/MainAreaActivity.java
@@ -1,17 +1,36 @@
 package com.areas.jr.areas;
 
+import android.app.ListActivity;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainAreaActivity extends ActionBarActivity {
+
+    AreaAdapter areaListAdapter;
+    ListView areaDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_area);
+
+        areaListAdapter = new AreaAdapter(this);
+
+        areaDesc = (ListView)findViewById(R.id.listView1);
+        areaDesc.setAdapter(areaListAdapter);
     }
 
 
@@ -35,5 +54,71 @@ public class MainAreaActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+ /*   public class AreaAdapter  extends BaseAdapter {
+
+        List<AreaInfo> areasList = getDataForListView();
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return areasList.size();
+        }
+
+        @Override
+        public AreaInfo getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return areasList.get(arg0);
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            // TODO Auto-generated method stub
+            return arg0;
+        }
+
+        @Override
+        public View getView(int arg0, View arg1, ViewGroup arg2) {
+            // TODO Auto-generated method stub
+            if(arg1==null)
+            {
+                LayoutInflater inflater = (LayoutInflater) MainAreaActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                arg1 = inflater.inflate(R.layout.area_row, arg2,false);
+            }
+
+            TextView chapterName = (TextView)arg1.findViewById(R.id.textView1);
+            TextView chapterDesc = (TextView)arg1.findViewById(R.id.textView2);
+
+            AreaInfo area = areasList.get(arg0);
+
+            chapterName.setText(area.name);
+            chapterDesc.setText(area.id);
+
+            return arg1;
+        }
+
+    }*/
+
+    public List<AreaInfo> getDataForListView()
+    {
+        List<AreaInfo> areaList = new ArrayList<AreaInfo>();
+
+        for(int i=0;i<10;i++)
+        {
+
+            AreaInfo area = new AreaInfo();
+            area.id = i;
+            area.name = "Repsol";
+            area.gas_station = 1;
+            area.garage = 1;
+            area.restaurant = 1;
+            area.situation = 0;
+            //area.position.latitude = 20.0;
+
+            areaList.add(area);
+        }
+
+        return areaList;
     }
 }
